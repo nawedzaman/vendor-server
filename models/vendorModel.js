32294d4dcb77e9ebe55c9b79f727a6152dfc8b00
nawedzaman <zamannawed28@gmail.com>
2023-05-18 13:23:51 +0530

    accept empty optional fields

--- models/vendorModel.js
+++ models/vendorModel.js
@@ -1,14 +1,25 @@
 const mongoose = require("mongoose");
 
 const vendorSchema = new mongoose.Schema({
-  vendorName: { type: String, required: true },
-  bankAccountNo: { type: String, required: true },
-  bankName: { type: String, required: true },
-  addressLine1: { type: String, required: true },
+  vendorName: {
+    type: String,
+    required: true,
+  },
+  bankAccountNo: {
+    type: String,
+    required: true,
+  },
+  bankName: {
+    type: String,
+    required: true,
+  },
+  addressLine1: String,
   addressLine2: String,
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-  zipCode: { type: String, required: true },
+  city: String,
+  country: String,
+  zipCode: String,
 });
 
-module.exports = mongoose.model("Vendor", vendorSchema);
+const Vendor = mongoose.model("Vendor", vendorSchema);
+
+module.exports = Vendor;
